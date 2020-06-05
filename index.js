d7c2d35be56711f40f39080cfbd526e743ef9d4c
Jessica <github email address>
2020-06-04 21:59:21 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+// #function takes in array
+// #filter through array and find if there is a 'W'
+// return year if won
+// return undefined if not won
+
+superbowlWin = (record) => {
+  let result = record.find(record => record.result === "W")
+  return result ? (result.year) : undefined
+};

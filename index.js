a24f6b3ab6205da0fbd5cccf80d70e9adbcd199f
blobbyblobfish <aywang6@gmail.com>
2020-06-08 16:47:23 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "W"},
+//   {year: "2016", result: "N/A"}
+//   //...
+// ]
+
+const recordArray = [
+  {year: "2000", result: "W"}
+]
+
+function superbowlWin(recordArray) {
+  // for each year, check if the result was "W"
+
+  let result = recordArray.find(record => record.result === "W")
+
+  // return the year if it found a matching result
+  return result ? result.year : undefined
+}
+
+superbowlWin(recordArray)
